bec56cc8a63cfc52510211fedf004c263303fda6
Arif Gencosmanoglu <a.gencosmanoglu@gmail.com>
2016-10-22 21:28:09 -0400

    Re added high lighting settings

--- app/src/main/java/com/a0014/unknownassistant/Models/CustomContactsListViewAdapter.java
+++ app/src/main/java/com/a0014/unknownassistant/Models/CustomContactsListViewAdapter.java
@@ -30,13 +30,18 @@ public class CustomContactsListViewAdapter extends ArrayAdapter<String>{
         View row = inflater.inflate(R.layout.custom_contacts_list_view, parent, false);
 
         TextView txtName = (TextView) row.findViewById(R.id.txt_name);
-        TextView txtPhoneNumber = (TextView) row.findViewById(R.id.txt_phone_number);
+        TextView txtPOhoneNumber = (TextView) row.findViewById(R.id.txt_phone_number);
 
         String name = strings.get(position).split(", ")[0];
         String phone = strings.get(position).split(", ")[1];
 
         txtName.setText("\t" + name);
-        txtPhoneNumber.setText("\t" + phone);
+        txtPOhoneNumber.setText("\t" + phone);
+
+        if(strings.get(position).contains("colorChanged")){
+            txtName.setBackgroundColor(0xFF233240);
+            txtPOhoneNumber.setBackgroundColor(0xFF1D2F40);
+        }
 
         return row;
     }
